376e9f881f4128b351628a8f8599024782e4315b
Evgeniy Remnev <evgenij-remnjov@mail.ru>
2015-11-25 23:38:21 +0300

    Реализована отправка писем без шифрования

--- SecureMail/Form1.Designer.cs
+++ SecureMail/Form1.Designer.cs
@@ -45,11 +45,16 @@
             this.label1 = new System.Windows.Forms.Label();
             this.statusStrip1 = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.statusStrip1.SuspendLayout();
+            this.groupBox4.SuspendLayout();
             this.SuspendLayout();
             // 
             // toolStripMenuItem1
@@ -118,7 +123,7 @@
             this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox1.Size = new System.Drawing.Size(593, 343);
+            this.groupBox1.Size = new System.Drawing.Size(593, 281);
             this.groupBox1.TabIndex = 1;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Отправка письма";
@@ -131,7 +136,7 @@
             this.groupBox3.Controls.Add(this.richTextBox1);
             this.groupBox3.Location = new System.Drawing.Point(8, 134);
             this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(577, 202);
+            this.groupBox3.Size = new System.Drawing.Size(577, 140);
             this.groupBox3.TabIndex = 4;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Текст письма";
@@ -142,14 +147,13 @@
             this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.richTextBox1.Location = new System.Drawing.Point(3, 20);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(571, 179);
+            this.richTextBox1.Size = new System.Drawing.Size(571, 117);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
             // groupBox2
             // 
-            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Controls.Add(this.textBox1);
@@ -159,7 +163,7 @@
             this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
-            this.groupBox2.Size = new System.Drawing.Size(575, 82);
+            this.groupBox2.Size = new System.Drawing.Size(575, 102);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Заголовок";
@@ -215,7 +219,8 @@
             // statusStrip1
             // 
             this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripStatusLabel1});
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
             this.statusStrip1.Location = new System.Drawing.Point(0, 380);
             this.statusStrip1.Name = "statusStrip1";
             this.statusStrip1.Size = new System.Drawing.Size(619, 22);
@@ -228,6 +233,52 @@
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(193, 17);
             this.toolStripStatusLabel1.Text = "Данные пользователя не введены";
             // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(184, 17);
+            this.toolStripStatusLabel2.Text = "Ключ шифрования не загружен";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox4.Controls.Add(this.button3);
+            this.groupBox4.Controls.Add(this.button1);
+            this.groupBox4.Location = new System.Drawing.Point(13, 316);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(593, 58);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Отправка сообщения";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(225, 24);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(229, 28);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Как есть";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.отправитьСообщениеКакЕсть);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(7, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Зашифрованным";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
@@ -235,9 +286,11 @@
             this.AutoScroll = true;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(619, 402);
+            this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(4);
@@ -252,6 +305,7 @@
             this.groupBox2.PerformLayout();
             this.statusStrip1.ResumeLayout(false);
             this.statusStrip1.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -276,6 +330,10 @@
         private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem6;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
     }
 }
 
--- SecureMail/Form1.cs
+++ SecureMail/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Security.Cryptography;
 using System.Web;
-using System.Net.Security;
+using System.Net;
 using System.Net.Mail;
 using System.Xml;
 using System.IO;
@@ -20,10 +20,6 @@ namespace SecureMail
     {
         const string filter = "rsa files (*.rsa)|*.rsa";
         RSACryptoServiceProvider csp;
-        RSAParameters key;
-        SmtpClient client;
-        MailAddress адресОтправителя;
-        MailMessage сообщение;
         struct ДанныеПользователя
         {
             private MailAddress адрес;
@@ -56,7 +52,7 @@ namespace SecureMail
         }
         ДанныеПользователя пользователь;
         public Form1()
-        {;
+        {
             InitializeComponent();
         }
 
@@ -100,6 +96,62 @@ namespace SecureMail
         { 
             пользователь.Адрес = адрес;
             пользователь.Пароль = пароль;
+            toolStripStatusLabel1.Text = "Данные пользователя введены.";
+            button3.Enabled = true;
         }
+        public void СменитьСтатусКлюча()
+        {
+            toolStripStatusLabel2.Text = "Ключ шифрования загружен.";
+            button1.Enabled = true;
+        }
+
+        private void отправитьСообщениеКакЕсть(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                try
+                {
+                    MailAddress получатель = new MailAddress(textBox1.Text);
+
+                    MailMessage сообщение = new MailMessage(пользователь.Адрес, получатель);
+                    сообщение.Subject = textBox2.Text;
+                    сообщение.Body = richTextBox1.Text;
+
+                    SmtpClient client = new SmtpClient("smtp." + пользователь.Адрес.Host, 25);
+                    client.Credentials = new NetworkCredential(пользователь.Адрес.Address, пользователь.Пароль);
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.EnableSsl = true;
+                    object obj = сообщение;
+                    client.SendCompleted += SendCompleted;
+                    client.SendAsync(сообщение, obj);
+
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show(this, exp.Message, "Ошибка!", MessageBoxButtons.OK,MessageBoxIcon.Error);
+                }                
+            }
+        }
+        void SendCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            MailMessage mail = (MailMessage)e.UserState;
+            string subject = mail.Subject;
+
+            if (e.Cancelled)
+            {
+                string cancelled = string.Format("[{0}] Отправка отменена.", subject);
+                MessageBox.Show(cancelled);
+            }
+            if (e.Error != null)
+            {
+                string error = String.Format("[{0}] {1}", subject, e.Error.ToString());
+                MessageBox.Show(error,"Ошибка!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+            else
+            {
+                MessageBox.Show("Сообщение успешно отправлено!","Уведомление.",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            }
+        }
+
     }
 }
