2c2900d998d81b366476ec88a8ceda66248d7265
Evgeniy Remnev <evgenij-remnjov@mail.ru>
2015-11-25 17:29:52 +0300

    Генерация ключей и запись в файл

--- SecureMail/Form1.cs
+++ SecureMail/Form1.cs
@@ -7,14 +7,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.Web;
+using System.Net.Security;
+using System.Net.Mail;
+using System.Xml;
+using System.IO;
 
 namespace SecureMail
 {
     public partial class Form1 : Form
     {
+        const string filter = "rsa files (*.rsa)|*.rsa";
+        RSACryptoServiceProvider csp;
+        RSAParameters key;
+        SmtpClient client;
+        MailAddress адресОтправителя;
+        MailMessage сообщение;
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.Description = "Выберите место для сохранения ключей";
+            fbd.ShowDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                if (csp==null)
+                {
+                    csp = new RSACryptoServiceProvider();
+                }
+                StreamWriter sw = new StreamWriter(fbd.SelectedPath+"\\OpenKey.rsa");
+                sw.WriteLine(csp.ToXmlString(false));
+                sw.Close();
+                sw = new StreamWriter(fbd.SelectedPath + "\\PrivateKey.rsa");
+                sw.WriteLine(csp.ToXmlString(true));
+                sw.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
     }
 }
